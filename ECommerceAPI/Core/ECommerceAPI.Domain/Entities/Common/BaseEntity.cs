97903617871b4a8dec76a8acf0a1305b5b215af3
Metehan Gülşer <mthnglsr@gmail.com>
2025-04-24 23:50:11 +0300

    Initial commit with clean architecture setup

---
+++ ECommerceAPI/Core/ECommerceAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime DeletedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ ECommerceAPI/Core/ECommerceAPI.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerceAPI.Domain.Entities.Common;
+
+namespace ECommerceAPI.Domain.Entities
+{
+    public sealed class Product : BaseEntity
+    {
+        public decimal Price { get; set; }
+        public decimal Stock { get; set; }
+    }
+}
